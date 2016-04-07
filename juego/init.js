26fdd91bd08cf355bd531d54b6003c15ef39f441
manu-msr <manu@ciencias.unam.mx>
2016-04-07 02:52:22 -0500

    Agrego comentarios y elimino archivos innecesarios.

--- juego/init.js
+++
@@ -1,3 +0,0 @@
-var gamevar = new Phaser.Game(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, Phaser.CANVAS,'gameContainer');
-gamevar.state.add('Load', Game.Load);
-gameContainer.add('MainMenu', Game.MainMenu);
--- juego/mainmenu.js
+++ juego/mainmenu.js
@@ -4,18 +4,25 @@ Game.MainMenu = function(game) {
 
 Game.MainMenu.prototype = {
 
-	/* Pantalla inicial. */
+	/* Pantalla inicial.  Esta función se encarga de crear toda la pantalla 
+	   inicial, indicando desde la dimensión hasta las imágenes o botones que
+	   tendrá.*/
 	create : function() {
+		// agregamos la imagen de fondo
 		this.game.add.tileSprite(0, 0, 1200, 800, 'background');
+		// modificamos las dimensiones
 		this.game.world.bounds.x = 0;
 		this.game.world.bounds.y = 0;
 		this.game.world.bounds.width = 600;
 		this.game.world.bounds.heigth = 800;
+		// agregamos el botón "jugar"
 		this.playbutton = this.add.button(600, 450, 'play', this.playclicked, this, 1, 0, 2);
 		this.playbutton.anchor.setTo(0.5, 0.5);
+		// Agregamos la animación al botón
 		this.tweenplay = this.game.add.tween(this.playbutton).to({y:500}, 1300, Phaser.Easing.Sinusoidal.InOut,true,0,100,true);
 	},
 
+	/* Función que realizará el botón al dar clic sobre el mismo. */
 	playclicked : function() {
 
 	},

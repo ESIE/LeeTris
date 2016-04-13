649731593da1c033789c2b6fc9330d1ad100a934
David Alejandro Nieto <davidalexito23@gmail.com>
2016-04-13 16:18:10 -0500

    Cambios a la pantalla de inicio

--- juego/load.js
+++ juego/load.js
@@ -8,7 +8,26 @@ Game.Load.prototype = {
 	/* Función que carga las imágenes necesarias para el juego. */
 	preload : function() {
 		this.load.image('background', 'assets/pantallaInicial.png');
-		this.load.spritesheet('play', 'assets/play.png', 300, 80);
+		this.load.spritesheet('mapa', 'assets/Mapa.png', 300, 80);
+		this.load.spritesheet('creditos', 'assets/Creditos.png', 300, 80);
+		this.load.spritesheet('instrucciones', 'assets/Instrucciones.png', 300, 80);
+		//botones de los niveles del mapa
+		this.load.image('boton1','assets/boton1.png');
+		this.load.image('boton2','assets/boton2.png');
+		this.load.image('boton3','assets/boton3.png');
+		this.load.image('boton4','assets/boton4.png');
+		this.load.image('boton5','assets/boton5.png');
+		this.load.image('boton6','assets/boton6.png');
+		this.load.image('boton7','assets/boton7.png');
+		this.load.image('boton8','assets/boton8.png');
+		this.load.image('boton9','assets/boton9.png');
+		this.load.image('boton10','assets/boton10.png');
+		this.load.image('boton11','assets/boton11.png');
+		this.load.image('boton12','assets/boton12.png');
+		this.load.image('boton13','assets/boton13.png');
+		this.load.image('boton14','assets/boton14.png');
+		this.load.image('boton15','assets/boton15.png');
+		this.load.image('boton16','assets/boton16.png');
 	},
 
 	/* Función que carga el juego. Para ello ubica el fondo de la pantalla 
--- juego/mainmenu.js
+++ juego/mainmenu.js
@@ -15,11 +15,17 @@ Game.MainMenu.prototype = {
 		this.game.world.bounds.y = 0;
 		this.game.world.bounds.width = 600;
 		this.game.world.bounds.heigth = 800;
-		// agregamos el botón "jugar"
-		this.playbutton = this.add.button(600, 450, 'play', this.playclicked, this, 1, 0, 2);
+		// agregamos el botón "Mapa"
+		this.playbutton = this.add.button(230, 550, 'mapa', this.playclicked, this, 1, 0, 2);
+		this.playbutton.anchor.setTo(0.5, 0.5);
+		// agregamos el botón "Instrucciones"
+		this.playbutton = this.add.button(600, 550, 'instrucciones', this.playclicked, this, 1, 0, 2);
+		this.playbutton.anchor.setTo(0.5, 0.5);
+		// agregamos el botón "Creditos"
+		this.playbutton = this.add.button(970, 550, 'creditos', this.playclicked, this, 1, 0, 2);
 		this.playbutton.anchor.setTo(0.5, 0.5);
 		// Agregamos la animación al botón
-		this.tweenplay = this.game.add.tween(this.playbutton).to({y:500}, 1300, Phaser.Easing.Sinusoidal.InOut,true,0,100,true);
+		
 	},
 
 	/* Función que realizará el botón al dar clic sobre el mismo. */
